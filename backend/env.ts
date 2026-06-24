fbc2811fd0dc52bb44523d3e9f05a6c1e58bb92d
Krish Ramani <krishramani03@gmail.com>
2026-06-24 14:39:35 +0530

    types for env variables done

--- backend/env.ts
+++ backend/env.ts
@@ -1,2 +1,52 @@
 import z from "zod";
 import { env as loadEnv } from "custom-env";
+
+process.env.APP_STAGE = process.env.APP_STAGE || "dev";
+
+const isProduction = process.env.APP_STAGE === "production";
+const isDevelopment = process.env.APP_STAGE === "dev";
+const isTesting = process.env.APP_STAGE === "test";
+
+if (isDevelopment) {
+  loadEnv();
+}
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  APP_STAGE: z.enum(["dev", "production", "test"]).default("dev"),
+  PORT: z.coerce.number().positive().default(3000),
+  DATABASE_URL: z.string().startsWith("postgres://"),
+  JWT_SECRET: z.string().min(32, "must be 32 characters long"),
+  JWT_EXPIRE_IN: z.string().default("7d"),
+  BCRYPT: z.coerce.number().min(10).max(20).default(12),
+});
+
+export type Env = z.infer<typeof envSchema>;
+let env: Env;
+
+try {
+  env = envSchema.parse(process.env);
+} catch (e) {
+  if (e instanceof z.ZodError) {
+    console.log("Invalid Env Variable");
+    console.error(JSON.stringify(e.flatten().fieldErrors, null, 2));
+
+    e.issues.forEach((err) => {
+      const path = err.path.join(".");
+      console.log(`${path}:${err.message}`);
+    });
+
+    process.exit(1);
+  }
+
+  throw e;
+}
+
+export const isProd = () => env.APP_STAGE === "production";
+export const isDev = () => env.APP_STAGE === "dev";
+export const isTest = () => env.APP_STAGE === "test";
+
+export { env };
+export default env;
